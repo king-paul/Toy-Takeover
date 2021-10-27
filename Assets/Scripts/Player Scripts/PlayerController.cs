9b36eb95d1c1c20a3c2ae7d25129e2d7f304b673
Paul King <PaulKing09@bigpond.com>
2021-10-27 12:39:45 +1100

    Implemented collection of remaining item pickups and added ammo drain
    - Health, Armour and ammo pickups now change values in the player controller
    - Ammo and Armour text has been added to the HUD
    - Firing a weapon now reduces ammo for that weapon

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,40 +4,110 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region variable declaration
     //public variables
+    [Header("Max Stats")]
     public float maxHealth = 100;
     public float maxArmour = 40;
     public float maxFuel = 4;
     [Range(0, 2)]
     public float fuelDrainSpeed = 1;
+
+    [Header("Weapons")]
     public GameObject[] weapons;
+
+    [Header("Fall off and spawning")]
     public Transform spawnPoint;
     public float fallOffDamage = 10f;
 
     // private variables
-    private float currentHealth;
+    [SerializeField]
+    private float currentHealth = 100;
     private float currentArmour = 0;
     private float currentFuel = 0;
     private bool hitFloor;
 
     private GameManager game;
     private GunController gun;
+    private WeaponController weapon;
 
     private int weaponNum = 0;
     private int prevWeaponNum;
+    #endregion
 
-    // properties
+    #region properties
     public float Health { get => currentHealth; }
     public float Fuel { get => currentFuel; }
+    public int Ammo {
+        get {
+            weapon = weapons[weaponNum].GetComponent<WeaponController>();
+            return weapon.Ammo;
+        }
+    }
+    public int MaxAmmo
+    {
+        get
+        {
+            weapon = weapons[weaponNum].GetComponent<WeaponController>();
+            return weapon.weaponObject.maxAmmo;
+        }
+    }
+    public float Armour { get => currentArmour; }
+    #endregion
 
+    #region public functions
+    // Setters
+    public void AddJetpackFuel(float amount)
+    {
+        currentFuel += amount;
+
+        if (currentFuel > maxFuel)
+            currentFuel = maxFuel;
+    }
+
+    public void AddHealth(float amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+    }
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth < 0)
+            currentHealth = 0;
+    }
+
+    public void AddArmour(float amount)
+    {
+        if(currentArmour < maxArmour)
+            currentArmour += amount;
+    }
+
+    public void AddAmmo(int weaponNum, int amount)
+    {
+        weapon = weapons[weaponNum].GetComponent<WeaponController>();
+        weapon.AddAmmo(amount);
+    }
+
+    public void DrainFuel()
+    {
+        currentFuel -= fuelDrainSpeed * Time.deltaTime;
+    }
+    #endregion
+
+    #region Unity functions
     // Awake is called as soon s the script is run
     void Awake()
     {
-        currentHealth = maxHealth;
+        //currentHealth = maxHealth;
         hitFloor = false;
         game = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
+    // Start is called after awake
     private void Start()
     {
         weapons[weaponNum].SetActive(true);
@@ -67,45 +137,34 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    public void AddJetpackFuel(float amount)
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        currentFuel += amount;
+        // send player to spawnpoint when they collide with floor
+        // and subtract health
+        if (hit.gameObject.layer == LayerMask.NameToLayer("Boundary") &&
+            hit.gameObject.CompareTag("KillZone"))
+        {
+            if (!hitFloor)
+            {
+                currentHealth -= fallOffDamage;
+                hitFloor = true;
 
-        if (currentFuel > maxFuel)
-            currentFuel = maxFuel;
+                Invoke("ResetHit", 0.5f); // prevents more than one collision
+
+                transform.position = spawnPoint.position;
+                transform.rotation = spawnPoint.rotation;
+            }
+        }
+
+        // projectile hits player
+        //if(hit.gameObject.CompareTag("Projectile"))
+        //{
+        //    currentHealth -= 10f;
+        //}
     }
+    #endregion
 
-    public void AddHealth(float amount)
-    {
-        currentHealth += amount;
-
-        if (currentHealth > maxHealth)
-            currentHealth = maxHealth;
-    }
-    public void TakeDamage(float amount)
-    {
-        currentHealth -= amount;
-
-        if (currentHealth < 0)
-            currentHealth = 0;
-    }
-
-    public void AddArmour(float amount)
-    {
-        if (currentArmour > maxArmour)
-            currentArmour = maxArmour;
-    }
-
-    public void AddAmmo(int weapon, float amount)
-    {
-
-    }
-
-    public void DrainFuel()
-    {
-        currentFuel -= fuelDrainSpeed * Time.deltaTime;
-    }
-
+    #region private functions
     private void SwitchWeapons()
     {
         if (weaponNum < 0)
@@ -120,46 +179,11 @@ public class PlayerController : MonoBehaviour
 
         Debug.Log("Selected weapon " + weaponNum);
     }
-   
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        /*
-        if(hit.gameObject.tag == "Item")
-        {
-            ItemController item = hit.gameObject.GetComponent<ItemController>();
 
-            GameObject.Destroy(hit.gameObject);
-            //Debug.Log("Player has picked up an item");
-        }
-        */
-
-        // send player to spawnpoint when they collide with floor
-        // and subtract health
-        if (hit.gameObject.layer == LayerMask.NameToLayer("Boundary") &&
-            hit.gameObject.CompareTag("KillZone"))
-        {
-            if (!hitFloor)
-            {
-                currentHealth -= fallOffDamage;
-                hitFloor = true;
-
-                Invoke("ResetHit", 0.5f); // prevents more than one collision
-            
-                transform.position = spawnPoint.position;
-                transform.rotation = spawnPoint.rotation;
-            }
-        }
-
-        // projectile hits player
-        //if(hit.gameObject.CompareTag("Projectile"))
-        //{
-        //    currentHealth -= 10f;
-        //}
-    }
-
-    void ResetHit()
+    private void ResetHit()
     {
         hitFloor = false;
     }
+    #endregion
 
 }
--- Assets/Scripts/Scriptable Objects/Weapon.cs
+++ Assets/Scripts/Scriptable Objects/Weapon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Weapon : ScriptableObject
 {
-    public float maxAmmo = 100;
+    public int maxAmmo = 100;
+    public int startingAmmo = 100;
     public float damagePerHit = 10;
 }
