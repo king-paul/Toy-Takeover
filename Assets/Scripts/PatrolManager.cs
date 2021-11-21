7b4a45ac0ca7b65eee2be2043d933859564a6136
Paul King <PaulKing09@bigpond.com>
2021-11-21 21:56:16 +1100

    Started creating new patrol manager and added it to the main scene
    Also deleted Main Scene Paul

---
+++ Assets/Scripts/PatrolManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PatrolRoute
+{
+    public Transform[] waypoints;
+    public bool twoWay;
+    public bool randomizeOrder;
+}
+
+
+public class PatrolManager : MonoBehaviour
+{
+    public PatrolRoute[] patrolRoutes;
+
+    public Transform[] GetPatrolRoutes()
+    {
+        int random = Random.Range(0, patrolRoutes.Length);
+        if (random == patrolRoutes.Length)
+            random = patrolRoutes.Length - 1;
+
+        return patrolRoutes[random].waypoints;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
