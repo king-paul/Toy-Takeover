43d14a887e1c1543dd8fcc688dc2ff67095e2543
lakak <itsmelakaiovens@gmail.com>
2021-11-26 22:10:29 +1100

    Fixed credits not playing correctly after certain actions

---
+++ Assets/Scripts/GUI Scripts/TimescaleSetter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimescaleSetter : MonoBehaviour
+{
+    public float timescaleToSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = timescaleToSet;
+    }
+}
