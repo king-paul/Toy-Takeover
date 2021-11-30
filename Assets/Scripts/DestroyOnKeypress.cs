620ffd83dca6de6eacf85c4290b14a40e3563dc4
Paul King <PaulKing09@bigpond.com>
2021-12-01 10:18:24 +1100

    Added title screen to game and updated credits screen
    - The game will go to title screen scene after splash screen
    - Pressing any key or button on the title screen scene will go to the main menu scene
    - A new skip button has been added to the credits screen
    - Pressing the cancel button (ESC if using keyboard) will go back to the main menu
    - Pressing down on the keyboard or game controller will speed up the credits.

---
+++ Assets/Scripts/DestroyOnKeypress.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnKeypress : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+            GameObject.Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/GUI Scripts/TimescaleSetter.cs
+++ Assets/Scripts/GUI Scripts/TimescaleSetter.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TimescaleSetter : MonoBehaviour
 {
     public float timescaleToSet;
+    public float maxTime = 10f;
+    public UnityEvent onCancelPressed;
+
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = timescaleToSet;
+        
     }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            onCancelPressed.Invoke();
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            if(Input.GetAxis("Vertical") < 0 && Time.timeScale <= 10)
+                Time.timeScale += -Input.GetAxis("Vertical");
+        }
+        else
+            Time.timeScale = timescaleToSet;
+    }
+
+
 }
