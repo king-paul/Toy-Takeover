c42e7455eabe8fccd5f1981c1f1a6ebe401a7e33
Paul King <PaulKing09@bigpond.com>
2021-12-05 21:57:37 +1100

    Deleted title screen screen and added title screen to main menu scene

--- Assets/Scripts/GUI Scripts/DestroyOnKeypress.cs
+++ Assets/Scripts/GUI Scripts/DestroyOnKeypress.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class DestroyOnKeypress : MonoBehaviour
 {
+    static bool showTitleScreen = true;
+
+    void Start()
+    {
+        if(!showTitleScreen)
+            GameObject.Destroy(this.gameObject);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.anyKey)
+        {
+            showTitleScreen = false;
             GameObject.Destroy(this.gameObject);
+        }
     }
 }
